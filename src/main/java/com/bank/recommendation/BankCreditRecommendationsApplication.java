957e1ae6fa9c1ead61fc74f3a34b961b2255194d
Arthur_Starovoyt <89992433888@mail.ru>
2026-02-10 21:43:23 +0300

    initial project setup with health endpoint

---
+++ src/main/java/com/bank/recommendation/BankCreditRecommendationsApplication.java
@@ -0,0 +1,13 @@
+package com.bank.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCreditRecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCreditRecommendationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/recommendation/BankCreditRecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCreditRecommendationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
